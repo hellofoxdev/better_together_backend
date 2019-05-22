2caaa6235594993337b2a067b310f943ff22d415
basti <sebastian.fox@me.com>
2019-05-22 22:49:37 +0200

    - remove wrong fields

--- src/main/java/com/sebastianfox/food/models/Invitation.java
+++ src/main/java/com/sebastianfox/food/models/Invitation.java
@@ -11,44 +11,12 @@ public class Invitation {
     @Column(name="invitation_id")
     private Integer id;
 
-    private String email;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    private User invited;
-
-    @OneToOne(fetch = FetchType.LAZY)
-    private User invitedBy;
-
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public User getInvited() {
-        return invited;
-    }
-
-    public void setInvited(User invited) {
-        this.invited = invited;
-    }
-
-    public User getInvitedBy() {
-        return invitedBy;
-    }
-
-    public void setInvitedBy(User invitedBy) {
-        this.invitedBy = invitedBy;
     }
 
 }
